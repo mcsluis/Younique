1ee9747369ba794d8541f61559659f265ab9e35e
Marc van der Sluis <mcsluis@gmail.com>
2026-06-21 20:31:01 +0200

    Initial Commit

---
+++ Kampert/KampertApp.swift
@@ -0,0 +1,17 @@
+//
+//  KampertApp.swift
+//  Kampert
+//
+//  Created by Marc van der Sluis on 21/06/2026.
+//
+
+import SwiftUI
+
+@main
+struct KampertApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
